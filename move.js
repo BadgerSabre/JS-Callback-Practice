35d44190431e21c15a1805ab7357211e886136d9
Brandon Dega <brandondega@gmail.com>
2022-03-25 12:55:38 -0500

    finished assignment

--- move.js
+++ move.js
@@ -1,3 +1,4 @@
+
 function move(element) {
     element.style.position = 'fixed'
 
@@ -6,7 +7,66 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveWithArrowKeys(left, bottom, callback) {
+        if (!callback) return;
+        let direction = null;
+        let x = left;
+        let y = bottom;
+        element.style.bottom = y + 'px'
+        element.style.left = x + 'px'
+    
+        setInterval(function() {
+                move(character).to(100, 250)
+                if(y < 250){
+                    character.style.zIndex = 1;
+                }
+                if(y > 250){
+                    character.style.zIndex = 0;
+                }
+
+                if(direction === 'west' && x > 0) {
+                    x -= 1
+                }
+                if(direction === 'north' && y + 75 < window.innerHeight) {
+                    y += 1
+                }
+                if(direction === 'east' && x + 50 < window.innerWidth) {
+                    x += 1
+                }
+                if(direction === 'south' && y - 100 > 0) {
+                    y -= 1
+                }
+                element.style.bottom = y + 'px'
+                element.style.left = x + 'px'
+            }
+        , 1)
+        
+        document.addEventListener('keydown', function(e) {
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowUp') {
+                direction = 'north'
+            }
+            if(e.key === 'ArrowLeft') {
+                direction = 'west'
+            }
+            if(e.key === 'ArrowRight') {
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown') {
+                direction = 'south'
+            }
+            callback(direction)
+        })
+        
+        document.addEventListener('keyup', function(e){
+            direction = null
+            callback(direction)
+        })
+    }
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
